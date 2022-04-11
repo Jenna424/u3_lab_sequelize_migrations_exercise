c5430667423c7c5a0536c5435fed84e69b0ae535
Jenna Leopold <jleopold424@gmail.com>
2022-04-11 11:37:40 -0400

    completed lab

---
+++ migrations/20220411151217-rename-Businesses-to-businesses.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.renameTable('Businesses', 'businesses')
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.renameTable('businesses', 'Businesses')
+  }
+}
---
+++ migrations/20220411152555-rename-phoneNmber-to-phoneNumber.js
@@ -0,0 +1,18 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.renameColumn(
+      'businesses',
+      'phoneNmber',
+      'phoneNumber'
+    )
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.renameColumn(
+      'businesses',
+      'phoneNumber',
+      'phoneNmber'
+    )
+  }
+}
